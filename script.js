4ba55367337ab765e5643702e79a3e6c7ef3d972
shristisaxena <117073006+shristisaxena@users.noreply.github.com>
2023-05-26 17:30:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function generateLuckyNumber() {
+    //  alert("You're inside javascript!");
+
+    //  generate a random number
+    let randomNumber = Math.floor(Math.random() * 10);
+
+    alert(randomNumber);
+}
+
+
+  
+  
